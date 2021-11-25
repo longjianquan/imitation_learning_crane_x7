d7f699118495bb31c9fa2261bb1b7c9c4f4bb0bf
yamaneco28 <ykouki111228@gmail.com>
2021-11-26 00:26:36 +0900

    update parameters of bilateral control

--- crane_x7/bilateral_control/crane_x7_setup.sh
+++
@@ -1,4 +0,0 @@
-sudo chmod 666 /dev/ttyUSB0
-sudo chmod 666 /dev/ttyUSB1
-sudo echo 1 > /sys/bus/usb-serial/devices/ttyUSB0/latency_timer
-sudo echo 1 > /sys/bus/usb-serial/devices/ttyUSB1/latency_timer
