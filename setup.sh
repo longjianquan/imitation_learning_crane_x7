4b0636d966fa16e8bc69dfb11ad4df8415b9fecf
yamaneco28 <ykouki111228@gmail.com>
2021-11-30 13:24:16 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-cd crane_x7/include
+cd ./crane_x7/include/
 git clone https://github.com/ROBOTIS-GIT/DynamixelSDK.git
-cd DynamixelSDK/c++/build/linux64
+cd ./DynamixelSDK/c++/build/linux64/
 make
-cd ../../../../
