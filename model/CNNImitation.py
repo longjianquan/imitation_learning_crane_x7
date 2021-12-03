37d4b92058edce015dff0d60017cb5a64ec9c2d9
yamaneco28 <ykouki111228@gmail.com>
2021-12-03 22:50:13 +0900

    add Conv4Imitation layer

--- model/CNNImitation.py
+++ model/CNNImitation.py
@@ -1,4 +1,43 @@
+import torch
 from torch import nn
+from torch.functional import Tensor
+
+
+class Conv4Imitation(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size: int = 3,
+        stride: int = 1,
+        padding: int = 2,
+        dropout: float = 0.0,
+    ):
+        super().__init__()
+
+        self.n_padding = padding
+
+        self.layer = nn.Sequential(
+            nn.Conv1d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+            ),
+            nn.Dropout(dropout),
+            nn.ReLU(),
+        )
+
+    def padding(self, x: Tensor):
+        replicate = torch.stack([x[:, :, 0]] * self.n_padding)
+        replicate = replicate.permute(1, 2, 0)
+        y = torch.cat([replicate, x], dim=2)
+        return y
+
+    def forward(self, x):
+        x = self.padding(x)
+        y = self.layer(x)
+        return y
 
 
 class CNNImitation(nn.Module):
@@ -13,16 +52,18 @@ class CNNImitation(nn.Module):
         conv_list = []
         for i in range(len(channels)-1):
             conv_list.append(
-                nn.Conv1d(
+                # nn.Conv1d(
+                Conv4Imitation(
                     channels[i],
                     channels[i+1],
-                    kernel_size=3,
-                    stride=1,
-                    padding=1,
-                    padding_mode='replicate',
+                    # kernel_size=3,
+                    # stride=1,
+                    # padding=1,
+                    # padding_mode='replicate',
+                    dropout=dropout,
                 ))
-            conv_list.append(nn.Dropout(dropout))
-            conv_list.append(nn.ReLU())
+            # conv_list.append(nn.Dropout(dropout))
+            # conv_list.append(nn.ReLU())
         self.conv = nn.Sequential(*conv_list)
         self.linear = nn.Linear(dim, dim)
 
